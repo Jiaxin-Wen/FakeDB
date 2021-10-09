51ef752cb96c066f88085f5a1151431a8ea45d55
zzx <zzxthu@163.com>
2021-10-09 21:41:29 +0800

    temp

--- fakedb/filesystem/buf_manager.py
+++ fakedb/filesystem/buf_manager.py
@@ -1,12 +1,13 @@
 import os
-
-from ..config import CACHE_SIZE
-
+import numpy as np
+from ..config import CACHE_SIZE, PAGE_SIZE
+from .lru import LRU
 
 class BufManager:
     def __init__(self):
-        pass
-
+        self.pages = np.zeros((CACHE_SIZE, PAGE_SIZE), dtype=np.uint8)
+        self.fdpd_to_idx = {}
+        self.lru = LRU()
 
     def close(self, fd):
         '''
@@ -20,4 +21,8 @@ class BufManager:
         '''
         写文件, 写到cache中
         '''
+        if fd not in self.fdpd_to_idx:
+            self.fdpd_to_idx[fd] = {}
+        if pd not in self.fdpd_to_idx[fd]:
+            pass
         pass
