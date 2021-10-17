ff76c52a808f199b296881a4c642eba6a464a495
xwwwwww <wenjx_thu@163.com>
2021-10-17 16:09:29 +0800

    update record_manager

--- fakedb/recordsystem/record_manager.py
+++ fakedb/recordsystem/record_manager.py
@@ -2,8 +2,9 @@
 
 
 from ..filesystem.file_manager import FileManager
-from .utils import get_record_size
-
+from .utils import get_record_capacity, get_bitmap_len
+from .header import Header
+from .filehandle import FileHandle
 
 class RecordManager:
 
@@ -12,7 +13,7 @@ class RecordManager:
 
     def create_file(self, filename, record_len):
         '''
-        TODO:
+        创建一张表
         filename: 待创建的文件名(数据库名+表名)
         record_len: 一条记录的长度
         '''
@@ -21,8 +22,17 @@ class RecordManager:
 
         # 添加头页
         fd = self.file_manager.open_file(filename) # 打开文件
-        record_size = get_record_size(record_len) # 计算记录最大条数
-
+        record_capacity = get_record_capacity(record_len) # 计算每页可存储记录的最大条数
+        bitmap_len = get_bitmap_len(record_capacity)
+        header = Header(
+            record_len = record_len, 
+            record_capacity = record_capacity, 
+            record_num = 0,
+            page_num = 1,
+            filename = filename,
+            bitmap_len = bitmap_len)
+        header_data = header.serialize()
+        self.file_manager.write_page(fd, 0, header_data) # 写在第一页
 
         # 关闭文件
         self.file_manager.close_file(filename)
@@ -34,6 +44,15 @@ class RecordManager:
     def open_file(self, filename):
         '''
         TODO:
-        打开文件
+        打开文件, 获得其句柄
         '''
         pass
+
+    def close_file(self, filename):
+        '''
+        TODO:
+        关闭文件
+        '''
+        fd = None
+        self.file_manager.close_file(fd)
+        
