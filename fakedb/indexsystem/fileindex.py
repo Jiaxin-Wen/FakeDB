16fd663810637ad29e03f7f1c0577dd11008e479
xwwwwww <wenjx_thu@163.com>
2021-11-28 13:03:59 +0800

    update fileindex

--- fakedb/indexsystem/fileindex.py
+++ fakedb/indexsystem/fileindex.py
@@ -6,10 +6,13 @@
 import numpy as np
 
 from .index_handler import IndexHandler
+from .node import TreeNode
 
 class FileIndex:
-    def __init__(self, handler):
-        pass
+    def __init__(self, handler, root_id):
+        self.root_id = root_id # 根节点的页号
+        self.handler = handler
+        self.root = TreeNode('inter', root_id, root_id, ) # TODO: 未完成
     
     
     
